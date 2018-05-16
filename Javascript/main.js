a32f94216e51943629c5530f173fc570ed11909f
ajaykian <shindeajay170@gmail.com>
2018-05-16 16:46:33 +0200

    exercices fait le 16/05

---
+++ Javascript/main.js
@@ -0,0 +1,30 @@
+
+var metier = "chauffeur";
+var pays = "Canada";
+var enfant = 2;
+var nom = "Aline";
+
+ console.log(nom);
+ console.log(metier);
+ console.log(pays);
+ console.log(enfant);
+
+ var a = 2018;
+ var b = 1977;
+ 
+ 
+ console.log(a - b);
+
+ var monAge = 14600;
+ var ageMax = 36500;
+ var denreeAlim = "boeuf";
+ var boeuf = 2;
+ var jourAn = 365;
+ console.log(monAge - ageMax / jourAn * boeuf);
+ console.log(denreeAlim);
+
+
+ étape 1: (3)*3+4/2;
+ étape 2: (3)*(7)/2;
+ étape 3: 21/2;
+ étape 4: 10,5
