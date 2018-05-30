fc52f7834fb2c9144aa5711faef6a28ea3d92cc8
ajaykian <shindeajay170@gmail.com>
2018-05-30 18:29:57 +0200

    ajut galerie photos

---
+++ ExercicesJS/main.js
@@ -0,0 +1,27 @@
+
+var metier = "chauffeur";
+var pays = "Canada";
+var enfant = 2;
+var nom = "Aline";
+
+ console.log(nom);
+ console.log(metier);
+ console.log(pays);
+ console.log(enfant);
+
+ var a = 2018;
+ var b = 1977;
+ 
+ 
+ console.log(a - b);
+
+ var monAge = 14600;
+ var ageMax = 36500;
+ var denreeAlim = "boeuf";
+ var boeuf = 2;
+ var jourAn = 365;
+ console.log(monAge - ageMax / jourAn * boeuf);
+ console.log(denreeAlim);
+
+ prompt("comment ça va aujourd'hui :)");
+ 
